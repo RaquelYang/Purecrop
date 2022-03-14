fdda6acb75d386d03ff8e8240a3b88b6a34826eb
RaquelYang <lorehmmoreh007@gmail.com>
2022-03-14 10:32:10 +0800

    del admin members

--- src/views/Back/Admin/Admin.vue
+++ src/views/Back/Admin/Admin.vue
@@ -49,13 +49,12 @@
 export default {
   data () {
     return {
-
       items: [
         { title: '商品管理', icon: 'mdi-corn', to: '/admin' },
         { title: '訂單管理', icon: 'mdi-clipboard-list-outline ', to: '/admin/orders' },
         { title: '輪播圖管理', icon: 'mdi-file-image-outline ', to: '/admin/swipers' },
-        { title: '最新消息管理', icon: 'mdi-newspaper-variant-outline', to: '/admin/news' },
-        { title: '會員管理', icon: 'mdi-account-outline ', to: '/admin' }
+        { title: '最新消息管理', icon: 'mdi-newspaper-variant-outline', to: '/admin/news' }
+        // { title: '會員管理', icon: 'mdi-account-outline ', to: '/admin' }
       ],
       right: null
     }
